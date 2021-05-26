80340dd11f75ea2a89bdce94cdb0097d286f78c7
Henrique Melanda <henrique.melanda@xpi.com.br>
2021-05-26 13:58:41 -0300

    Learn NextJS, GraphQL and Leaflet in practice

---
+++ next-seo.config.ts
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://my-trips.henriquemelanda.com.br',
+    site_name: 'My Trips'
+  },
+  twitter: {
+    handle: '@coisadedev',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
